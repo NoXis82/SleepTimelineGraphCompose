034dd3b2dccb85184b4416ba9c6c31e69f38d252
Tumanov Vycheslav <63115274+NoXis82@users.noreply.github.com>
2025-02-03 22:03:04 +0300

    add draw text title

--- app/src/main/java/ru/noxis/sleeptimelinegraphcompose/components/SleepSessionCanvas.kt
+++ app/src/main/java/ru/noxis/sleeptimelinegraphcompose/components/SleepSessionCanvas.kt
@@ -2,6 +2,7 @@ package ru.noxis.sleeptimelinegraphcompose.components
 
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -12,12 +13,16 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.drawText
+import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import kotlinx.datetime.Instant
 import ru.noxis.sleeptimelinegraphcompose.model.SleepSessionRecord
 import ru.noxis.sleeptimelinegraphcompose.model.SleepSessionStageType
-import ru.noxis.sleeptimelinegraphcompose.model.SleepStageDrawPoint
+import kotlin.time.Duration.Companion.minutes
 
 @Composable
 fun SleepSessionCanvas(
@@ -25,6 +30,9 @@ fun SleepSessionCanvas(
     record: SleepSessionRecord,
     stageHeight: Dp = 48.dp,
     stagesSpacing: Dp = 16.dp,
+    textSpacing: Dp = 4.dp,
+    typeTextStyle: TextStyle = LocalTextStyle.current,
+    timeTextStyle: TextStyle = LocalTextStyle.current,
 ) {
     val colors = remember {
         mapOf(
@@ -34,26 +42,49 @@ fun SleepSessionCanvas(
             SleepSessionStageType.REM to Color(0xFF795548),
         )
     }
+    val textMeasurer = rememberTextMeasurer()
 
     val stageHeightPx = with(LocalDensity.current) { stageHeight.toPx() }
     val stagesSpacingPx = with(LocalDensity.current) { stagesSpacing.toPx() }
+    val textSpacingPx = with(LocalDensity.current) { textSpacing.toPx() }
 
+    val typeTextHeight = with(LocalDensity.current) {
+        remember {
+            textMeasurer.measure("", style = typeTextStyle).size.height
+        }.toDp()
+    }
+
+    val timeTextHeight = with(LocalDensity.current) {
+        remember {
+            textMeasurer.measure("", style = timeTextStyle).size.height
+        }.toDp()
+    }
     Spacer(
         modifier = modifier
-            .requiredHeight(stageHeight * colors.size + stagesSpacing * (colors.size - 1))
+            .requiredHeight((stageHeight + typeTextHeight + textSpacing) * colors.size + stagesSpacing * (colors.size - 1) + textSpacing + timeTextHeight)
             .drawWithCache {
-
                 val stages = listOf(
                     SleepSessionStageType.Awake,
                     SleepSessionStageType.REM,
                     SleepSessionStageType.Light,
                     SleepSessionStageType.Deep,
                 ).map { type ->
-                    type to calculate(
-                        canvasSize = size.copy(height = stageHeightPx),
-                        recordStartTime = record.startTime,
-                        recordEndTime = record.endTime,
-                        stages = record.stages.filter { it.type == type },
+                    val stages = record.stages.filter { it.type == type }
+                    val stageDuration =
+                        stages.sumOf { (it.endTime - it.startTime).inWholeMinutes }.minutes
+                    DrawStage(
+                        title = textMeasurer.measure(
+                            text = "$type + $stageDuration",
+                            style = typeTextStyle,
+                            maxLines = 1
+                        ),
+                        type = type,
+                        points = calculatePoints(
+                            canvasSize = size.copy(height = stageHeightPx),
+                            recordStartTime = record.startTime,
+                            recordEndTime = record.endTime,
+                            stages = record.stages.filter { it.type == type },
+                        )
                     )
                 }
 
@@ -66,7 +97,12 @@ fun SleepSessionCanvas(
 
                 onDrawWithContent {
                     var offset = 0f
-                    stages.forEach { (type, points) ->
+                    stages.forEach { (title, type, points) ->
+                        // Draw title (type and duration)
+                        translate(top = offset) {
+                            drawText(title)
+                        }
+                        offset += title.size.height + textSpacingPx
                         translate(top = offset) {
                             // Draw background
                             drawRoundRect(
@@ -98,12 +134,12 @@ fun SleepSessionCanvas(
  * topOffset = ((stage startTime) - (session startTime)) / session duration * canvas.width
  * size = stage duration * canvas.width
  */
-private fun calculate(
+private fun calculatePoints(
     canvasSize: Size,
     recordStartTime: Instant,
     recordEndTime: Instant,
     stages: List<SleepSessionRecord.Stage>,
-): List<SleepStageDrawPoint> {
+): List<DrawPoint> {
     val totalDuration = (recordEndTime - recordStartTime).inWholeSeconds.toFloat()
         .coerceAtLeast(1f)
 
@@ -113,9 +149,20 @@ private fun calculate(
         val stageDuration =
             (stage.endTime - stage.startTime).inWholeSeconds.toFloat() / totalDuration
 
-        SleepStageDrawPoint(
+        DrawPoint(
             topLeft = Offset(x = canvasSize.width * stageOffset, y = 0f),
             size = canvasSize.copy(width = canvasSize.width * stageDuration),
         )
     }
 }
+
+private data class DrawPoint(
+    val topLeft: Offset,
+    val size: Size,
+)
+
+private data class DrawStage(
+    val title: TextLayoutResult,
+    val type: SleepSessionStageType,
+    val points: List<DrawPoint>,
+)
--- app/src/main/java/ru/noxis/sleeptimelinegraphcompose/model/SleepStageDrawPoint.kt
+++
@@ -1,9 +0,0 @@
-package ru.noxis.sleeptimelinegraphcompose.model
-
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
-
-data class SleepStageDrawPoint(
-    val topLeft: Offset,
-    val size: Size
-)
